e1c6f951d7579186537b8c4faeeae2bb92b660b6
Drew McDermott <code@drewmcdermott.net>
2017-09-20 12:41:46 -0400

    Config with TOML

--- Listeners/jabberListener.py
+++ Listeners/jabberListener.py
@@ -8,25 +8,36 @@ class JabberListener(Listener):
 
     listenerType = ListenerType.JABBER
 
-    def __init__(self, jid, password, host, port):
-        print("Client for: " + jid)
-        self.client =  ClientXMPP(jid, password)
+    def __init__(self, config):
+        self.config = config
+        self.name = config['name']
+        self.jid = config['jid']
+        self.password = config['password']
+        self.host = config['host']
+        self.port = config['port']
+        self.client =  ClientXMPP(self.jid, self.password)
         self.client.add_event_handler("session_start", self.onConnect)
         self.client.add_event_handler("message", self.parseMessage)
-        self.host = host
-        self.port = port
+        self.client.register_plugin("xep_0045")  # Multi-User Chat
 
     def connect(self):
         print("Connecting... " + self.host + ":" + str(self.port))
-        self.client.connect((self.host, self.port))
+        try:
+            self.client.connect((self.host, self.port))
+        except Exception as err:
+            print("Could not connect to Jabber host: {0}".format(err))
         self.client.process()
 
+    def disconnect(self):
+        self.client.disconnect()
+
     def onConnect(self, event):
-        self.client.send_presence()
+        self.client.sendPresence()
         print("Connected")
         pprint(event)
 
     def parseMessage(self, msg):
+        print("Got message")
         if self.messageHandler is None:
             return
 
--- main.py
+++ main.py
@@ -1,10 +1,35 @@
+#!/usr/bin/python
+
 from Listeners.jabberListener import JabberListener
+from Listeners.listenerType import ListenerType
 from pprint import pprint
+from pathlib import Path
+import toml
+import os
 
 def main():
-    jab = JabberListener("made you", "look", "conference.goonfleet.com", "5222")
-    jab.onMessage(message)
-    jab.connect();
+    #Import config from the TOML file
+    dir = os.path.dirname(__file__)
+    file_path = os.path.join(dir, 'Config.toml')
+    config_file = Path(file_path).read_text()
+    config = toml.loads(config_file)
+    pprint(config)
+    startListeners(config['listeners'])
+
+
+def startListeners(listeners):
+    for l in listeners:
+        try:
+            listenType = ListenerType[l['type'].upper()]
+            listener = None
+            if(listenType == ListenerType.JABBER):
+                listener = JabberListener(l)
+            listener.onMessage(message)
+            listener.connect()
+        except Exception as e:
+            print(e)
+            pass
+
 
 def message(msg):
     pprint(msg)
