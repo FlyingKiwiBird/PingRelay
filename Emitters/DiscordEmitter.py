8b5d961d110e24850d283e29345e942a7647f08d
Flying Kiwi <fweddit@flyingkiwibird.com>
2019-08-09 18:31:10 -0400

    Update discord

--- Emitters/DiscordEmitter.py
+++ Emitters/DiscordEmitter.py
@@ -78,10 +78,10 @@ class DiscordEmitter(Emitter):
                     if message.has_alert:
                         content = "@everyone"
                     _log.debug("Using embed method")
-                    await self.client.send_message(channel, content=content, embed=message.embed())
+                    await channel.send_message(content=content, embed=message.embed())
                 else:
                     _log.debug("Using text method")
-                    await self.client.send_message(channel, content=message_str)
+                    await channel.send_message(content=message_str)
                 _log.debug("{0} - Sent message to channel '{1}' on '{2}'".format(self.name, channel.name, channel.server.name))
             except Exception as err:
                 _log.error("{0} - Could not send message: {1}".format(self.name, err))
